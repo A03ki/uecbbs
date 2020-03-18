7c39985a145038f611343cb3002af509d511ab08
A03ki <a03ki04@gmail.com>
2020-03-18 20:33:07 +0900

    Tweepyで取得したタイムラインに使う関数の追加  #1

---
+++ twissify/api.py
@@ -0,0 +1,51 @@
+def has_media(tweet):
+    return "media" in tweet.entities
+
+
+def is_photo(tweet):
+    """
+    Note: Twitterの仕様上、動画やGIFは複数投稿できないため、最初のmediaのみを確認している
+    """
+    if has_media(tweet):
+        return tweet.extended_entities["media"][0]["type"] == "photo"
+    return False
+
+
+def is_retweet(tweet):
+    return hasattr(tweet, "retweeted_status")
+
+
+def filter_myretweeted_tweets(tweets):
+    "自身がリツイート済みのツイートを取り除く"
+    return [tweet for tweet in tweets if not tweet.retweeted]
+
+
+def filter_retweets(tweets):
+    "リツイートを取り除く"
+    return [tweet for tweet in tweets if not is_retweet(tweet)]
+
+
+def extract_public_tweets(tweets):
+    "鍵垢以外ののツイートを取り出す"
+    return [tweet for tweet in tweets if not tweet.user.protected]
+
+
+def extract_photo_tweets(tweets):
+    "画像のツイートを取り出す"
+    return [tweet for tweet in tweets if is_photo(tweet)]
+
+
+def extract_tweet_ids(tweets):
+    "ツイートIDを取り出す"
+    return [tweet.id for tweet in tweets]
+
+
+def extract_photo_urls(photo_tweet):
+    "画像のツイートに含まれる複数の画像のurlを取り出す"
+    return [media["media_url"]
+            for media in photo_tweet.extended_entities["media"]]
+
+
+def extract_photos_urls(tweets):
+    "それぞれのツイートに含まれる複数の画像のurlを取り出す"
+    return [extract_photo_urls(tweet) for tweet in tweets if is_photo(tweet)]
