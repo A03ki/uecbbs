aabbd1640b4ef7cc46efdde51d637853a9a0bc8b
A03ki <a03ki04@gmail.com>
2020-04-11 22:07:24 +0900

    画像urlを渡すとPIL画像を返す関数の追加 #5

---
+++ twissify/image.py
@@ -0,0 +1,29 @@
+import io
+import PIL
+import requests
+
+
+def load_image_url(image_url):
+    """画像urlからImageオブジェクトとHTTPステータスコードを得る
+
+    画像urlに正常にアクセスできたときはImageオブジェクトとHTTPステータスコードを得る
+    正常にアクセスできなかったときはImageオブジェクトの代わりにNoneが返る
+
+    Parameters
+    ----------
+    image_url : str
+        画像urlの文字列
+
+    Returns
+    -------
+    tuple of a inheritance of PIL.ImageFile.ImageFile and int
+        Imageオブジェクト(またはNone)とHTTPステータスコードのタプル
+
+    Note: urlが存在しないときはConnectionErrorが呼ばれる
+        また、画像url以外のurlではUnidentifiedImageErrorが呼ばれる
+    """
+    image = None
+    response = requests.get(image_url)
+    if response.status_code == 200:
+        image = PIL.Image.open(io.BytesIO(response.content))
+    return image, response.status_code
