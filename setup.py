519a18fc20c85700ab0476fd8ba140cea2224572
A03ki <a03ki04@gmail.com>
2020-05-15 14:11:54 +0900

    SQLAlchemyパッケージを追加

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version="0.5.0",
     description="The package to classify the photo tweet",
     author="A03ki",
-    install_requires=["numpy", "pillow", "tweepy"],
+    install_requires=["numpy", "pillow", "sqlalchemy", "tweepy"],
     url="",
     license="MIT License",
     packages=["twissify",
